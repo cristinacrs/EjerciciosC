2d75c0ad0397fb31b44d2ffeaa7a53bc26482172
Cristina Cruz Cortés <cristinacruzcortes@gmail.com>
2023-05-30 20:04:08 -0600

    positivo y negativo

---
+++ EjercicioPositivoNegativo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	float a,b;
+	
+	printf("Ingresa el numero: ");
+	scanf("%f", &a);
+
+	
+	if(a<0){
+		printf("El numero es negativo");
+	}else if(a>0){
+		printf("El número es positivo");
+	}
+	
+}
